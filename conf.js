37ed4e4a7991bf5d946b89c7983227b19e2b9a63
Bobo <33951304+qupb@users.noreply.github.com>
2019-06-17 10:00:54 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -30,7 +30,7 @@ jsproxy_config({
       lines: [
         // 实验中...
         // 参考 https://github.com/EtherDream/jsproxy/tree/master/cf-worker
-        'node-cfworker.etherdream.com:8443'
+        'https://damp-hat-336a.bobqu.workers.dev'
       ]
     }
   },
